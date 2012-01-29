cb019b53e5c45d4da1a4d2272ee62c156e27da9b
Shota Fukumori (sora_h) <sorah@tubusu.net>
2012-01-29 18:34:55 +0900

    pass spec

--- lib/kubot/bot.rb
+++ lib/kubot/bot.rb
@@ -1,11 +1,43 @@
 module Kubot
   class Bot
+    class << self
+      def responders
+        @responders ||= []
+      end
+
+      def respond(*conds, &block)
+        responders << [conds, block]
+        self
+      end
+
+      def reset
+        @responders = []
+        self
+      end
+    end
+
+    def initialize(options={},config={})
+      @options = options
+      @config = config
+    end
+
+    attr_reader :config
+
+    def db; @options[:db]; end
+
+    def fire(message)
+      self.class.responders.each do |responder|
+        matchs = message.match(*responder[0])
+        responder[1].yield self, message, matchs if matchs
+      end
+      self.respond_to?(method = :"on_#{message.type}") && self.__send__(method, message)
+      self
+    end
+
     def bot_name
       self.class.name \
           .gsub(/([A-Z]{2,}?)([a-z0-9])/){ $1.downcase + "_" + $2 } \
           .gsub(/([a-z0-9])([A-Z])/){ $1 + "_" + $2.downcase }.downcase
     end
-
-
   end
 end
