a5f36a7e60d2b2e2277bf5e7d47f97130345ef1e
Shota Fukumori (sora_h) <sorah@tubusu.net>
2012-02-19 14:19:37 +0900

    add mock path for test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,7 @@
 require 'rspec'
 $:.unshift File.expand_path("#{File.dirname(__FILE__)}/../lib")
+
+module Kubot
+  MOCK_PATH = "#{File.dirname(__FILE__)}/mock_bots"
+  MOCK_LOAD_PATH = [MOCK_PATH]
+end
