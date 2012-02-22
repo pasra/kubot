69745cbae5a4743dc73bb5b440a11ecc725552eb
Shota Fukumori (sora_h) <sorah@tubusu.net>
2012-02-22 21:02:30 +0900

    fix bugs of spec

--- spec/manager_spec.rb
+++ spec/manager_spec.rb
@@ -18,10 +18,18 @@ describe Kubot::Manager do
   end
 
   describe '.new' do
-    it 'accepts Array and Hash as load_path, bot list' do
+    it 'accepts Array as load_path, bot list' do
       expect { Kubot::Manager.new(Kubot::MOCK_LOAD_PATH) }.to_not raise_error
     end
-  end 
+  end
+
+  describe '#locate' do
+    it 'returns bot file name found in load_path' do
+      subject.locate(:foo).should == "#{Kubot::MOCK_PATH}/foo.rb"
+      subject.locate(:bar).should == "#{Kubot::MOCK_PATH}/bar.rb"
+      subject.locate(:shouldnt_exist).should be_nil
+    end
+  end
 
   describe '#load' do
     it 'accepts bot name, config, option' do
@@ -30,27 +38,33 @@ describe Kubot::Manager do
       expect { subject.load(:foo, {the: :config}, {db: nil}) }.to_not raise_error
     end
 
-    it 'finds bot using load_path and start it' do
+    it 'finds bot using #locate start it' do
       config= {the: :config}
       option= {db: nil}
-      Kernel.should_receive(:load).and_return { FooBot.should_receive(:new).with(config, option).and_return(nil) }
+      dummy = "this is a dummy"
+      subject.should_receive(:locate).with(:foo).and_return(dummy)
+      FooBot.should_receive(:new).with(config, option).and_return(nil)
+      Kubot::Bot.stub(:bots_diff).and_yield.and_return([FooBot])
+      Kernel.should_receive(:load).with(dummy)
+      File.stub(:mtime){Time.now}
       subject.load(:foo, config, option)
     end
 
     it 'detects bot using bots_diff' do
       Kubot::Bot.should_receive(:bots_diff).and_return([FooBot])
-      FooBot.should_receive(:new).and_return(FooBot.new)
+      bot = FooBot.new
+      FooBot.should_receive(:new).and_return(bot)
       subject.load(:foo)
     end
 
     it 'reloads bot using #reload if it has already loaded' do
-      should_not_receive(:reload)
+      subject.should_receive(:reload).with(:foo, {the: :config}, {}).once
       subject.load(:foo, {the: :config})
-      should_receive(:reload).with(:foo, {the: :config}).and_return(subject)
       subject.load(:foo, {the: :config})
     end
 
     it 'raises error unless bot file is found' do
+      subject.stub(:locate){nil}
       expect { subject.load(:this_is_not_exist) }.to raise_error(Kubot::Manager::BotNotFound)
     end
 
@@ -67,9 +81,11 @@ describe Kubot::Manager do
     it 'starts bots both already exist and added' do
       bot_a = [EaBot.new, EaBot.new]
       bot_b = EbBot.new
+
       Kubot::Bot.stub(:bots_diff) { [EaBot] }
       EaBot.should_receive(:new).and_return(bot_a.shift)
       subject.load(:exist)
+
       Kubot::Bot.stub(:bots_diff) { [EbBot] }
       EaBot.should_receive(:new).and_return(bot_a.shift)
       EbBot.should_receive(:new).and_return(bot_b)
@@ -81,7 +97,8 @@ describe Kubot::Manager do
         Kubot::Bot.stub(:bots_diff) { [MaBot, MbBot] }
 
         [MaBot, MbBot].each do |klass|
-          klass.should_receive(:new).and_return(klass.new)
+          bot = klass.new
+          klass.should_receive(:new).and_return(bot)
         end
 
         subject.load(:multi)
@@ -91,7 +108,7 @@ describe Kubot::Manager do
 
   describe '#unload' do
     it 'raises error unless specified bot has loaded' do
-      expect { Kubot::Manager.new.unload(:foo) }.to raise_error(Kubot::Manager::BotNotLoaded)
+      expect { subject.unload(:foo) }.to raise_error(Kubot::Manager::BotNotLoaded)
     end
 
     it 'calls Bot#finalize to specified bot' do
@@ -136,60 +153,52 @@ describe Kubot::Manager do
 
   describe '#reload' do
     it 'calls #unload first then call #load' do
-      should_receive(:unload).with(:not_exist).ordered
-      should_receive(:load).with(:not_exist).ordered
+      subject.should_receive(:unload).with(:not_exist).ordered
+      subject.should_receive(:load).with(:not_exist, {}, {}).ordered
 
       subject.reload(:not_exist)
     end
 
     context 'when calling #unload' do
       it 'ignores Kubot::Manager::BotNotLoaded error' do
-        should_receive(:unload) { raise Kubot::Manager::BotNotLoaded }
+        subject.should_receive(:unload) { raise Kubot::Manager::BotNotLoaded }
         subject.stub(:load) {}
         expect { subject.reload(:foo) }.to_not raise_error
       end
     end
-  end
-
-  describe '#reload' do
-    before :each do
-      @tmpdir = Dir.mktmpdir
-      FileUtils.cp("#{Kubot::MOCK_PATH}/reload_a.rb", @tmpdir)
-      FileUtils.cp("#{Kubot::MOCK_PATH}/reload_b.rb", @tmpdir)
-    end
-
-    subject { Kubot::Manager.new([@tmpdir]) }
 
     it 'calls #load for all plugins which newer than when loaded before' do
+      path_a = Kubot::MOCK_PATH+"/reload_a.rb"
+      path_b = Kubot::MOCK_PATH+"/reload_b.rb"
+      time_a = File.mtime(path_a)
+      time_b = File.mtime(path_b)
+      future_a = time_a+100
+      future_b = time_b+100
+
+      File.should_receive(:mtime).with(path_a).and_return(time_a, future_a, future_a)
+      File.should_receive(:mtime).with(path_b).and_return(time_b, future_b, future_a)
+
       subject.load(:reload_a)
       subject.load(:reload_b)
 
-      should_not_receive(:load)
-      should_not_receive(:unload)
+      subject.should_receive(:unload).with(:reload_a).twice.ordered
+      subject.should_receive(:load).with(:reload_a, {}, {}).twice.ordered
+      subject.should_receive(:unload).with(:reload_b).twice.ordered
+      subject.should_receive(:load).with(:reload_b, {}, {}).twice.ordered
+
       subject.reload
-
-      FileUtils.touch "#{@tmpdir}/reload_a.rb"
-      FileUtils.touch "#{@tmpdir}/reload_b.rb"
-
-      should_receive(:unload).with(:reload_a)
-      should_receive(:unload).with(:reload_b)
-      should_receive(:load).with(:reload_a)
-      should_receive(:load).with(:reload_b)
       subject.reload
-    end
-
-    after :each do
-      FileUtils.remove_entry_secure @tmpdir
     end
   end
 
   describe '#unload_all' do
     it 'unloads all bots that has an instance using #unload' do
       foo = FooBot.new
-      FooBot.stub(:new){foo}
+      FooBot.stub(:new).and_return foo
       bar = BarBot.new
-      BarBot.stub(:new){bar}
+      BarBot.stub(:new).and_return bar
 
+      Kubot::Bot.stub(:bots_diff).and_return([FooBot, BarBot])
       subject.load(:foo)
       subject.load(:bar)
 
